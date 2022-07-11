cff6e0b88dc048bde6f8350e9ec54e938a178441
Tabish <tabishabbasi3@gmail.com>
2022-07-11 09:24:07 -0400

    Initial Commit

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import database.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/LoginForm.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        JDBC.makeConnection();
+        launch();
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,14 @@
+module C195Project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    opens main to javafx.fxml;
+    exports main;
+    opens controllers to javafx.fxml;
+    exports controllers;
+    opens model to javafx.fxml;
+    exports model;
+    opens database to javafx.fxml;
+    exports database;
+}
